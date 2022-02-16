b00ee7c0abb6665b456a36fa37d5b6a9c911511d
njneubauer <njneubauer@gmail.com>
2022-02-15 21:14:26 -0600

    began acceptance testing. Completed filterEventsByCity BDD testing. Completed end-to-end test using puppeteer for show/hide details button

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
-import CitySearch from '../CitySearch';
 import mockData from '../mockData';
 
 describe('<NumberOfEvents /> Component',()=>{
---
+++ src/features/endToEnd.test.js
@@ -0,0 +1,36 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+    let browser, page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250
+        });
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.Event');
+    });
+
+    afterAll(()=>{
+        browser.close();
+    });
+
+    test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeNull();
+    });
+
+    test('User can expand an event to see its details', async () => {
+        await page.click('.Event .details-btn');
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeDefined();
+    });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.Event .details-btn');
+        const eventDetails = await page.$('.Event .details');
+        expect(eventDetails).toBeNull();
+    });
+});
