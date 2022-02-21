fd03c4045f38ba7d96736a09893a1aafc3f49a31
njneubauer <njneubauer@gmail.com>
2022-02-20 23:07:13 -0600

    added 2 alerts for city input & number of events input

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component{
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
     }
 
     handleFocusToggle = (event)=>{
@@ -21,23 +22,34 @@ class CitySearch extends Component{
             });
 
         }
-    }
+    };
 
     handelInputChanged = (event) =>{
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location)=>{
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         })
-        this.setState({
-            query: value,
-            suggestions,
-            showSuggestions: true
-        });
-    }
+        
+        if(suggestions.length === 0){
+            this.setState({
+                query: value,
+                suggestions,
+                infoText: "We can not find the city you are looking for. Please try another city."
+            });
+        }
+        else {
+            this.setState({
+                query: value,
+                suggestions,
+                infoText: ''
+            });
+        }
+    };
 
     handleItemClicked = (suggestion, reset)=>{
         this.setState({
             query: suggestion,
+            infoText: ''
         });
         if(reset === true){
             this.props.updateEvents({location: suggestion, eventCount: 32});
@@ -45,28 +57,29 @@ class CitySearch extends Component{
         else{
             this.props.updateEvents({location: suggestion});
         }
-    }
+    };
 
     render(){
         return (
             <>
-            <div className="CitySearch">
-                <h3>Choose your nearest city</h3>
-                <input id="city" className="city" type="text" placeholder="Search City" value={this.state.query} 
-                    onFocus={this.handleFocusToggle} 
-                    onBlur={this.handleFocusToggle} 
-                    onChange={this.handelInputChanged} 
-                />
-                <ul id="suggestions" className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }} >
-                    {this.state.suggestions.map((suggestion)=>(
-                        <li key={suggestion} onMouseDown={()=>this.handleItemClicked(suggestion)}>{suggestion}</li>
-                    ))}
-                    <li onMouseDown={()=>this.handleItemClicked('')} key='all-cities'>
-                        <b>see all cities</b>
-                    </li>
-                </ul>
-            </div>
-            <button id="clearCity" className="reset-btn" onClick={()=>this.handleItemClicked('', true)}>Reset</button>
+                <div className="CitySearch">
+                    <InfoAlert text={this.state.infoText} className={'Alert-info'} />
+                    <h3>Choose your nearest city</h3>
+                    <input id="city" className="city" type="text" placeholder="Search City" value={this.state.query} 
+                        onFocus={this.handleFocusToggle} 
+                        onBlur={this.handleFocusToggle} 
+                        onChange={this.handelInputChanged} 
+                    />
+                    <ul id="suggestions" className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }} >
+                        {this.state.suggestions.map((suggestion)=>(
+                            <li key={suggestion} onMouseDown={()=>this.handleItemClicked(suggestion)}>{suggestion}</li>
+                        ))}
+                        <li onMouseDown={()=>this.handleItemClicked('')} key='all-cities'>
+                            <b>see all cities</b>
+                        </li>
+                    </ul>
+                </div>
+                <button id="clearCity" className="reset-btn" onClick={()=>this.handleItemClicked('', true)}>Reset</button>
             </>
         );
     };
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-
+import { ErrorAlert }from './Alert';
 
 class NumberOfEvents extends Component {    
+    state={
+        errorText: ''
+    }
 
     handleInputChange = (event)=>{
         const newNumber = event.target.value;
-        this.props.updateEvents({eventCount: newNumber});
+        if(newNumber > 32 || newNumber < 1){
+            this.props.updateEvents({eventCount: newNumber});
+            this.setState({errorText: 'Select a number between 1 to 32'});
+        }
+        else {
+            this.props.updateEvents({eventCount: newNumber});
+            this.setState({errorText: ''});
+
+        }
     }
     
     render(){
@@ -13,6 +24,7 @@ class NumberOfEvents extends Component {
             <div className="NumberOfEvents">
                 <h4>Number of Events:</h4>
                 <input id="eventNumber" className="number" type='number' value={this.props.eventCount} onChange={this.handleInputChange} />
+                <ErrorAlert text={this.state.errorText} className={'Alert-error'} />
             </div>
         )
     }
