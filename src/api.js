527323cd07ce8fdc222bceec4e93b8e6af1cc83c
njneubauer <njneubauer@gmail.com>
2022-02-21 20:07:55 -0600

    updated api function getEvents() to load localstorage data if offline

--- src/api.js
+++ src/api.js
@@ -75,6 +75,12 @@ export const getEvents = async () => {
     return mockData
   }
 
+  if(!navigator.onLine){
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(data).events : [];;
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -86,6 +92,7 @@ export const getEvents = async () => {
       localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
     }
+
     NProgress.done();
     return result.data.events;
   }
